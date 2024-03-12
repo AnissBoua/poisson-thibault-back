7367acfbcd639be6297b2c90087539fc0881dd24
Tran-Hugo <82827738+Tran-Hugo@users.noreply.github.com>
2024-03-12 09:28:04 +0100

    wip realtime ca

--- quickstart/view/ca.py
+++ quickstart/view/ca.py
@@ -2,9 +2,11 @@ import json
 from django.core import serializers
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from quickstart.consumers import WSConsumer
 from quickstart.models import TransactionProduit
 from quickstart.services.CAService import CAService
-
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
 class CAEndpoints(APIView):
     def get(self, request, pk=None, format=None):
         startStr = request.GET.get('start')
@@ -25,6 +27,8 @@ class CAEndpoints(APIView):
         transactionProduits = CAService.getTransactionProduitsBy(**params)
                 
         cas = CAService.getCA(transactionProduits, startStr, endStr)
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)("ca_group", {"type": "send_message", "message": "Nouveau CA"})
         
         return Response(cas)
         
